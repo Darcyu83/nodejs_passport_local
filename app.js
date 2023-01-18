947b589b9dc69d5071c280cafca1252ed3193f88
Darcyu83 <darcy.u83@gmail.com>
2023-01-18 16:34:17 +0900

    서버 베이스 구현

--- app.js
+++ app.js
@@ -12,13 +12,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
     secret: "yuds_secret",
+    resave: false,
+    saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 2,
     },
   })
 );
 
-app.get("/", (req, res) => {
+function isAuthenticatedMiddleware(req, res, next) {
+  console.log("req.headers.cookie", req.headers.cookie);
+  if (req.headers.cookie) {
+  }
+  next();
+}
+
+app.get("/", isAuthenticatedMiddleware, (req, res) => {
+  // 로그인 성공 시 serialize하고 req.session.passport.user 에 담김
+  // 로그인 성공하고 deserialize하고 req.user 에 담김
+
   res.send("Hi");
 });
 
